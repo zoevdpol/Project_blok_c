4033f793393436b45478a4dc630d6d10664f3205
Jorn Roosendahl <jornroosendahl@hotmail.com>
2022-03-16 10:28:32 +0100

    test

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jornr on 16/03/2022.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by jornr on 16/03/2022.
+//
+
+#ifndef PROJECT_BLOK_C_MAIN_H
+#define PROJECT_BLOK_C_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PROJECT_BLOK_C_MAIN_H
